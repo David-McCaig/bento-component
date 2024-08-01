f4d20839d5a5165b695b4a4ff5f711c28e3d0134
David-McCaig <davidmccaig1@gmail.com>
2024-08-01 16:43:12 +1200

    create bento component

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+'use client'
+import Image from "next/image";
+import {Bento} from "../components/bento"
+
+export default function Home() {
+  return (
+    <main className="">
+      <Bento/>
+    </main>
+  );
+}
